04fc5d9e23b42eacbd9da600345929e86578473c
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-01-28 18:07:18 +0300

    add test clases for lesson 3

---
+++ src/main/java/base/lesson3/IndexPage.java
@@ -0,0 +1,34 @@
+package base.lesson3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class IndexPage {
+
+    private WebElement userIcon;
+    private WebElement userField;
+    private WebElement passwordField;
+    private WebElement submitButton;
+
+    private WebDriver driver;
+
+    public IndexPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void login(String name, String password) {
+        userIcon = driver.findElement(By.cssSelector("[id='user-icon']"));
+        userIcon.click();
+        userField = driver.findElement(By.cssSelector("[id='name']"));
+        userField.sendKeys(name);
+        passwordField = driver.findElement(By.cssSelector("[id='password']"));
+        passwordField.sendKeys(password);
+        submitButton = driver.findElement(By.cssSelector("[id='login-button']"));
+        submitButton.click();
+    }
+
+    public void open() {
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+    }
+}
