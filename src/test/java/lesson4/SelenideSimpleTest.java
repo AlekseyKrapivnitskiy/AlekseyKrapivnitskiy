7111de64411786c9cf5afeab3c6a0df9258647c6
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-01-30 18:08:04 +0300

    add selenide tests

---
+++ src/test/java/lesson4/SelenideSimpleTest.java
@@ -0,0 +1,27 @@
+package lesson4;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+
+public class SelenideSimpleTest {
+
+
+    @Test
+    public void simpleTest() {
+        //1
+        open("https://epam.github.io/JDI/index.html");
+
+        //2
+        Assert.assertEquals(getWebDriver().getTitle(), "Home Page");
+
+        //3
+
+        $("[id='user-icon']").click();
+        $("[id='name']").sendKeys("epam");
+        $("[id='password']").sendKeys("1234");
+        $("[id='login-button']").click();
+    }
+}
