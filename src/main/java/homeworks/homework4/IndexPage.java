102bb6b81978734474a767e88d85911b6dbb7c5d
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-08 00:57:19 +0300

    add tests

--- src/main/java/homeworks/homework4/IndexPage.java
+++ src/main/java/homeworks/homework4/IndexPage.java
@@ -3,10 +3,17 @@ package homeworks.homework4;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class IndexPage {
+
+    public void open(String url) {
+        Selenide.open(url);
+    }
 
     public void login(String name, String password) {
         SelenideElement userIcon = $("#user-icon");
@@ -19,11 +26,19 @@ public class IndexPage {
         submitButton.click();
     }
 
-    public void open(String url) {
-        Selenide.open(url);
-    }
-
     public void checkTitle(String title) {
         $("title").shouldHave(attribute("text", title));
+    }
+
+    public void checkUsername(String username) {
+        $("#user-name").shouldHave(text(username));
+    }
+
+    public void checkServiceDropDownList() {
+        List<String> serviceDropDown = $(".dropdown-menu > li").;
+
+        for (int i = 0; i < serviceDropDown.size(); i ++) {
+            assertEquals(serviceDropDown.get(i).getText(), headerItems.get(i));
+        }
     }
 }
--- src/test/java/homeworks/homework4/ServicePageInterfaceCheck.java
+++ src/test/java/homeworks/homework4/ServicePageInterfaceCheck.java
@@ -22,5 +22,34 @@ public class ServicePageInterfaceCheck extends SelenideBase {
         indexPage.login(PITER_CHAILOVSKII.login, PITER_CHAILOVSKII.password);
 
         //4.Assert User name in the left-top side of screen that user is logged in
+        indexPage.checkUsername(PITER_CHAILOVSKII.username);
+
+        //5.Click on "Service" subcategory in the header and check that drop down contains options
+
+        //6.Click on Service subcategory in the left section and check that drop down contains options
+
+        //7.Open through the header menu Service -> Different Elements Page
+
+        //8.Check interface on Different elements page, it contains all needed elements
+
+        //9.Assert that there is Right Section
+
+        //10.Assert that there is Left Section
+
+        //11.Select checkboxes
+
+        //12.Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+
+        //13.Select radio
+
+        //14.Assert that for radiobutton there is a log row and value is corresponded to the status of radiobutton
+
+        //15.Select in dropdown
+
+        //16.Assert that for dropdown there is a log row and value is corresponded to the selected value
+
+        //17.Unselect and assert checkboxes
+
+        //18.Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox
     }
 }
