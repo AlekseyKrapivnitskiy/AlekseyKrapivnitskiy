4ef93a246df19c394bcb576af95efe941b1b4a54
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-09 19:31:32 +0300

    add selenide configuration

--- src/main/java/base/lessons/lesson4/SelenideBase.java
+++ src/main/java/base/lessons/lesson4/SelenideBase.java
@@ -1,5 +1,6 @@
 package base.lessons.lesson4;
 
+import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 
 import static java.lang.System.setProperty;
@@ -9,5 +10,7 @@ public abstract class SelenideBase {
     @BeforeSuite
     public void beforeSuite() {
         setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        Configuration.browser = "chrome";
+        Configuration.startMaximized = true;
     }
 }
