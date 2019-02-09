d09ea8fb656aadf2a3eb1e59c52d6833563592a6
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-09 20:01:37 +0300

    delete static import

--- src/main/java/base/lessons/lesson4/SelenideBase.java
+++ src/main/java/base/lessons/lesson4/SelenideBase.java
@@ -3,13 +3,11 @@ package base.lessons.lesson4;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 
-import static java.lang.System.setProperty;
-
 public abstract class SelenideBase {
 
     @BeforeSuite
     public void beforeSuite() {
-        setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         Configuration.browser = "chrome";
         Configuration.startMaximized = true;
     }
