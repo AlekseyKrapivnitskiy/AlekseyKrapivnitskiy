ebd4d2bfa9c853107d63a05f2dc0c5f727a9cbdd
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-10 21:59:51 +0300

    add enums for checkboxes and radio buttons

---
+++ src/main/java/homeworks/homework4/enums/Checkboxes.java
@@ -0,0 +1,15 @@
+package homeworks.homework4.enums;
+
+public enum Checkboxes {
+
+    WATER("Water"),
+    EARTH("Earth"),
+    WIND("Wind"),
+    FIRE("Fire");
+
+    public String label;
+
+    Checkboxes(String label) {
+        this.label = label;
+    }
+}
---
+++ src/main/java/homeworks/homework4/enums/RadioButtons.java
@@ -0,0 +1,15 @@
+package homeworks.homework4.enums;
+
+public enum  RadioButtons {
+
+    GOLD("Gold"),
+    SILVER("Silver"),
+    BRONZE("BRONZE"),
+    SELEN("Selen");
+
+    public String label;
+
+    RadioButtons(String label) {
+        this.label = label;
+    }
+}
