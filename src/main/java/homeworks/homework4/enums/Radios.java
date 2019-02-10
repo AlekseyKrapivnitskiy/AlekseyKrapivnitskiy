125f6c3781fe07d768df80f716046cb3221064f1
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-10 11:20:13 +0300

    add enum for colors

---
+++ src/main/java/homeworks/homework4/enums/Radios.java
@@ -0,0 +1,15 @@
+package homeworks.homework4.enums;
+
+public enum Radios {
+
+    GOLD("Gold"),
+    SILVER("Silver"),
+    BRONZE("Bronze"),
+    SELEN("Selen");
+
+    public String text;
+
+    Radios(String text) {
+        this.text = text;
+    }
+}
