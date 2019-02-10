4ece34bf346cb35678ccfb3196cc122fd8078425
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-10 10:42:06 +0300

    add enum for checkboxes labels

---
+++ src/main/java/homeworks/homework4/enums/SupportCheckboxes.java
@@ -0,0 +1,15 @@
+package homeworks.homework4.enums;
+
+public enum SupportCheckboxes {
+
+    WATER("Support"),
+    EARTH("Dates"),
+    WIND("Complex Table"),
+    FIRE("Simple Table");
+
+    public String text;
+
+    SupportCheckboxes(String text) {
+        this.text = text;
+    }
+}
