fbf850a157fc6ce413b6157b0f1cd060225f8534
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-10 20:30:14 +0300

    fix page factory

--- src/main/java/homeworks/homework3/IndexPage.java
+++ src/main/java/homeworks/homework3/IndexPage.java
@@ -4,7 +4,6 @@ import homeworks.homework3.enums.CenterTexts;
 import homeworks.homework3.enums.IndexPageData;
 import homeworks.homework3.enums.JdiGithub;
 import homeworks.homework3.enums.Users;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -35,6 +34,39 @@ public class IndexPage {
     @FindBy(css = "#login-button")
     private WebElement submitButton;
 
+    @FindBy(css = "#user-name")
+    private WebElement usernameLabel;
+
+    @FindBy(css = ".uui-navigation.nav.navbar-nav.m-l8 > li")
+    private List<WebElement> navigationBar;
+
+    @FindBy(css = ".benefit-icon")
+    private List<WebElement> benefitIcons;
+
+    @FindBy(css = ".benefit-txt")
+    private List<WebElement> benefitTxt;
+
+    @FindBy(css = "#iframe")
+    private WebElement iFrame;
+
+    @FindBy(css = ".main-title.text-center")
+    private WebElement centerTextTitle;
+
+    @FindBy(css = ".main-txt.text-center")
+    private WebElement centerText;
+
+    @FindBy(css = "#epam_logo")
+    private WebElement epamLogo;
+
+    @FindBy(css = ".text-center > a")
+    private WebElement jdiGithubLink;
+
+    @FindBy(css = "#mCSB_1")
+    private WebElement leftMenu;
+
+    @FindBy(css = "footer")
+    private WebElement footer;
+
     // TODO Why parameter is String -- fixed
     public void open(IndexPageData indexPageData) {
         driver.navigate().to(indexPageData.url);
@@ -55,63 +87,57 @@ public class IndexPage {
 
     // TODO Why parameter is String -- fixed
     public void checkUsername(Users users) {
-        assertEquals(driver.findElement(By.cssSelector("#user-name")).getText(), users.username);
+        assertEquals(usernameLabel.getText(), users.username);
     }
 
     public void checkHeaderItems(List<String> headerItems) {
-        List<WebElement> navigationBar = driver.findElements(By.cssSelector(".uui-navigation.nav.navbar-nav.m-l8 > li"));
-
         for (int i = 0; i < navigationBar.size(); i ++) {
             assertEquals(navigationBar.get(i).getText(), headerItems.get(i));
         }
     }
 
     public void checkBenefitIcons() {
-        List<WebElement> benefitIcons = driver.findElements(By.cssSelector(".uui-navigation.nav.navbar-nav.m-l8 > li"));
-
         for (WebElement benefitIcon : benefitIcons) {
             assertTrue(benefitIcon.isDisplayed());
         }
     }
 
     public void checkBenefitTexts(List<String> benefitTexts) {
-        List<WebElement> benefitTxt = driver.findElements(By.cssSelector(".benefit-txt"));
-
         for (int i = 0; i < benefitTxt.size(); i ++) {
             assertEquals(benefitTxt.get(i).getText(), benefitTexts.get(i));
         }
     }
 
     public void checkCenterText(CenterTexts centerTexts) {
-        assertEquals(driver.findElement(By.cssSelector(".main-title.text-center")).getText(), centerTexts.title);
-        assertEquals(driver.findElement(By.cssSelector(".main-txt.text-center")).getText(), centerTexts.mainText);
+        assertEquals(centerTextTitle.getText(), centerTexts.title);
+        assertEquals(centerText.getText(), centerTexts.mainText);
     }
 
     public void checkIFrame() {
-        assertTrue(driver.findElement(By.cssSelector("#iframe")).isDisplayed());
+        assertTrue(iFrame.isDisplayed());
     }
 
     public void switchToIFrame() {
-        driver.switchTo().frame(driver.findElement(By.cssSelector("#iframe")));
+        driver.switchTo().frame(iFrame);
     }
 
     public void checkLogoInIframe() {
-        assertTrue(driver.findElement(By.cssSelector("#epam_logo")).isDisplayed());
+        assertTrue(epamLogo.isDisplayed());
     }
 
     public void checkSubHeader(JdiGithub jdiGithub) {
-        assertEquals(driver.findElement(By.cssSelector(".text-center > a")).getText(), jdiGithub.title);
+        assertEquals(jdiGithubLink.getText(), jdiGithub.title);
     }
 
     public void checkSubHeaderLink(JdiGithub jdiGithub) {
-        assertEquals(driver.findElement(By.cssSelector(".text-center > a")).getAttribute("href"), jdiGithub.url);
+        assertEquals(jdiGithubLink.getAttribute("href"), jdiGithub.url);
     }
 
     public void checkLeftMenu() {
-        assertTrue(driver.findElement(By.cssSelector("#mCSB_1")).isDisplayed());
+        assertTrue(leftMenu.isDisplayed());
     }
 
     public void checkFooter() {
-        assertTrue(driver.findElement(By.cssSelector("footer")).isDisplayed());
+        assertTrue(footer.isDisplayed());
     }
 }
