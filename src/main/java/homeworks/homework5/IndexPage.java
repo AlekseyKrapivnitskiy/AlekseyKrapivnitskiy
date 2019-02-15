957f576ad5cf1c1549cebe5b4d473f98538df033
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-15 12:24:26 +0300

    add steps to index page

--- src/main/java/homeworks/homework5/IndexPage.java
+++ src/main/java/homeworks/homework5/IndexPage.java
@@ -4,6 +4,7 @@ import base.homeworks.homework4.BasePage;
 import com.codeborne.selenide.SelenideElement;
 import homeworks.homework5.enums.IndexPageData;
 import homeworks.homework5.enums.Users;
+import io.qameta.allure.Step;
 import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.text;
@@ -27,11 +28,12 @@ public class IndexPage extends BasePage {
     @FindBy(css = "#user-name")
     private SelenideElement usernameLabel;
 
+    @Step("2.Assert Browser title")
     public void checkTitle(IndexPageData indexPageData) {
-        // TODO Why you use this method? What was the concern with the getTitle()? -- fixed
         assertEquals(getWebDriver().getTitle(), indexPageData.title);
     }
 
+    @Step("3.Perform login")
     public void login(Users users) {
         userIcon.click();
         userField.sendKeys(users.login);
@@ -39,6 +41,7 @@ public class IndexPage extends BasePage {
         submitButton.click();
     }
 
+    @Step("4.Assert User name in the left-top side of screen that user is logged in")
     public void checkUsername(Users users) {
         usernameLabel.shouldHave(text(users.username));
     }
