924a6041cee9a0f7d4645e0736a22715810bb3bb
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-15 13:08:35 +0300

    add steps to index page

--- src/main/java/homeworks/homework5/DatesPage.java
+++ src/main/java/homeworks/homework5/DatesPage.java
@@ -4,6 +4,7 @@ import base.homeworks.homework5.BasePage;
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import homeworks.homework5.enums.Sliders;
+import io.qameta.allure.Step;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 
@@ -23,11 +24,13 @@ public class DatesPage extends BasePage {
 
     private Actions actions = new Actions(getWebDriver());
 
+    @Step("6.Using drag-and-drop set Range sliders:{sliders} {value}")
     public void moveSlider(Sliders sliders, int value) {
         actions.clickAndHold(slider.get(sliders.index)).moveToElement(sliderLine,  ((sliderLine.getSize().width) * (value)/100 ),
                 0).release().build().perform();
     }
 
+    @Step("11.Assert that for \"From\" and \"To\" sliders there are logs rows with corresponding values")
     public void checkSliderLogs(String direction, Integer value) {
         logsPanel.shouldHave(text("Range 2(" + direction + "):" + value + " link clicked"));
     }
