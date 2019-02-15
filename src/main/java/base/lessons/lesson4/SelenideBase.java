30a7f5ce8709281f57a2df0d3076bd52bf78965b
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-15 11:35:26 +0300

    configure test for homework 5

--- src/main/java/base/lessons/lesson4/SelenideBase.java
+++ src/main/java/base/lessons/lesson4/SelenideBase.java
@@ -1,6 +1,5 @@
 package base.lessons.lesson4;
 
-import com.codeborne.selenide.Browsers;
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeSuite;
 
@@ -8,9 +7,7 @@ public abstract class SelenideBase {
 
     @BeforeSuite
     public void beforeSuite() {
-        // TODO Why you set this property???
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        Configuration.browser = Browsers.CHROME;
+        // TODO Why you set this property??? -- fixed
         Configuration.startMaximized = true;
     }
 }
