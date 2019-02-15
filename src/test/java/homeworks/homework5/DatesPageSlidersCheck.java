35028e157f1d8b3b9f015390254364fc79028df6
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-15 12:46:30 +0300

    add steps to index page

--- src/test/java/homeworks/homework5/DatesPageSlidersCheck.java
+++ src/test/java/homeworks/homework5/DatesPageSlidersCheck.java
@@ -33,7 +33,7 @@ public class DatesPageSlidersCheck extends SelenideBase {
         close();
     }
 
-    @Test(description = "Dates Page Sliders Check")
+    @Test(testName = "Dates Page Sliders Check")
     public void datesPageSlidersTest() {
         //2.Assert Browser title
         indexPage.checkTitle(INDEX_PAGE_DATA);
