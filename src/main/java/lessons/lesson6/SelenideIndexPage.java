2c09a3a915774dcb3ea2635e15812bd9c9b82fef
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-19 11:51:01 +0300

    add cucumber tests from lesson

--- src/main/java/lessons/lesson6/SelenideIndexPage.java
+++ src/main/java/lessons/lesson6/SelenideIndexPage.java
@@ -1,8 +1,13 @@
 package lessons.lesson6;
 
 import com.codeborne.selenide.SelenideElement;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import homeworks.homework5.enums.Users;
 import org.openqa.selenium.support.FindBy;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SelenideIndexPage {
@@ -22,6 +27,7 @@ public class SelenideIndexPage {
     @FindBy(css = "#user-name")
     private SelenideElement usernameLabel;
 
+    @When("I login as user 'epam' with password '1234'")
     public void login(String name, String password) {
         userIcon.click();
         userField.sendKeys(name);
@@ -29,7 +35,13 @@ public class SelenideIndexPage {
         submitButton.click();
     }
 
+    @Given("I open EPAM JDI site")
     public void openPage() {
         open("https://epam.github.io/JDI/index.html");
+    }
+
+    @Then("User name should be 'PITER CHAILOVSKII'")
+    public void checkUsername(Users users) {
+        usernameLabel.shouldHave(text(users.username));
     }
 }
