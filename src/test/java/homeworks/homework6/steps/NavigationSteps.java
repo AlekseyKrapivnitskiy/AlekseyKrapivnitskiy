c59a906d25b2b47d55e6725c2f3e5cc8d266ee0c
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-20 11:25:50 +0300

    refactoring

--- src/test/java/homeworks/homework6/steps/NavigationSteps.java
+++ src/test/java/homeworks/homework6/steps/NavigationSteps.java
@@ -1,10 +1,10 @@
-package homeworks.homework6;
+package homeworks.homework6.steps;
 
 import cucumber.api.java.en.Given;
 
 import static com.codeborne.selenide.Selenide.open;
 
-public class MyStepdefs {
+public class NavigationSteps {
     @Given("^I am on (.*)$")
     public void iAmOnHttpsEpamGithubIoJDI(String url) {
         open(url);
