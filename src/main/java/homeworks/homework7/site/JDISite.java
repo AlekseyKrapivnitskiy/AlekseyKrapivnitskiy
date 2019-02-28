0c364af5c7da922d94cc2ebc6115ecd5929652b0
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-02-28 18:53:24 +0300

    add url annotation for metals and colors page

--- src/main/java/homeworks/homework7/site/JDISite.java
+++ src/main/java/homeworks/homework7/site/JDISite.java
@@ -3,10 +3,14 @@ package homeworks.homework7.site;
 import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
 import homeworks.homework7.site.pages.HomePage;
+import homeworks.homework7.site.pages.MetalsAndColosPage;
 
 @JSite("https://epam.github.io/JDI/")
 public class JDISite {
 
     @Url("index.html")
     public static HomePage homePage;
+
+    @Url("metals-colors.html")
+    public static MetalsAndColosPage metalsAndColosPage;
 }
