d14a7fe5f28f647a811e08239aad69458946e715
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-05 22:22:30 +0300

    fix to do

--- src/main/java/homeworks/homework7/site/pages/MetalsAndColorsPage.java
+++ src/main/java/homeworks/homework7/site/pages/MetalsAndColorsPage.java
@@ -3,7 +3,6 @@ package homeworks.homework7.site.pages;
 import com.epam.jdi.light.elements.composite.WebPage;
 import homeworks.homework7.enums.*;
 import homeworks.homework7.site.forms.MetalsAndColorsForm;
-import homeworks.homework7.site.sections.*;
 import org.openqa.selenium.WebElement;
 
 import java.util.ArrayList;
@@ -16,40 +15,33 @@ public class MetalsAndColorsPage extends WebPage {
     /*
     TODO All of this elements should be inside of the form,
          in the other words, form should consist of this elements.
-    TODO This is not make sense to create a section with one particular element...
+    TODO This is not make sense to create a section with one particular element... -- fixed
      */
-    private Summary summary;
-    private Elements element;
-    private Colors colors;
-    private Metals metals;
-    private Vegetables vegetables;
-    private Result result;
-
     private MetalsAndColorsForm metalsAndColorsForm;
 
     public void selectSummary(SummaryRadiobuttons summaryRadiobuttons) {
         if(summaryRadiobuttons.value % 2 == 0) {
-            summary.odd.select(summaryRadiobuttons.label);
+            metalsAndColorsForm.odd.select(summaryRadiobuttons.label);
         }
         else {
-            summary.even.select(summaryRadiobuttons.label);
+            metalsAndColorsForm.even.select(summaryRadiobuttons.label);
         }
     }
 
     public void selectElement(ElementsList elementsList) {
-        element.elements.select(elementsList.label);
+        metalsAndColorsForm.elements.select(elementsList.label);
     }
 
     public void selectColor(ColorsList color) {
-        colors.colorsDropdown.select(color.label);
+        metalsAndColorsForm.colorsDropdown.select(color.label);
     }
 
     public void selectMetal(MetalsList metal) {
-        metals.metalDropdown.select(metal.label);
+        metalsAndColorsForm.metalsDropdown.select(metal.label);
     }
 
     public void selectVegetable(VegetablesList vegetable) {
-        vegetables.vegetablesDropdown.select(vegetable.label);
+        metalsAndColorsForm.vegetablesDropdown.select(vegetable.label);
     }
 
     public void submitMetalsAndColorsForm() {
@@ -59,7 +51,7 @@ public class MetalsAndColorsPage extends WebPage {
     public void checkResults(ResultsList resultsList, String resultValue) {
         // TODO Take a look on stream::map method
         List<String> resultContent = new ArrayList<>();
-        for (WebElement element : result.resultSection) {
+        for (WebElement element : metalsAndColorsForm.resultSection) {
             resultContent.add(element.getText());
         }
         assertTrue(resultContent.contains(resultsList.label + resultValue));
