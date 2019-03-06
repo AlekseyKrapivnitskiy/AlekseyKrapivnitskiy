8e8cd446b6be340731b09b750489d09eb055c28b
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-06 22:37:05 +0300

    fix to do

--- src/main/java/homeworks/homework7/entities/MetalsAndColors.java
+++ src/main/java/homeworks/homework7/entities/MetalsAndColors.java
@@ -6,13 +6,32 @@ import homeworks.homework7.enums.Vegetables;
 
 import java.util.List;
 
+import static homeworks.homework7.enums.Colors.RED;
+import static homeworks.homework7.enums.Elements.FIRE;
+import static homeworks.homework7.enums.Elements.WATER;
+import static homeworks.homework7.enums.Metals.SELEN;
+import static homeworks.homework7.enums.Summary.EIGHT;
+import static homeworks.homework7.enums.Summary.THREE;
+import static homeworks.homework7.enums.Vegetables.CUCUMBER;
+import static homeworks.homework7.enums.Vegetables.TOMATO;
+import static java.util.Arrays.asList;
+
 public class MetalsAndColors {
 
-    public static MetalsAndColors homeworkSeven = new MetalsAndColors();
+    public MetalsAndColors homeworkSeven = new MetalsAndColors(asList(THREE.label, EIGHT.label),
+            asList(WATER.label, FIRE.label), RED.label, SELEN.label, asList(CUCUMBER.label, TOMATO.label));
 
-    List<Summary> summary;
-    List<Elements> elements;
-    List<Vegetables> vegetables;
-    String metal;
-    String color;
+    private List<String> summary;
+    private List<String> elements;
+    private String color;
+    private String metal;
+    private List<String> vegetables;
+
+    public MetalsAndColors(List<String> summary, List<String> elements, String color, String metal, List<String> vegetables) {
+        this.summary = summary;
+        this.elements = elements;
+        this.color = color;
+        this.metal = metal;
+        this.vegetables = vegetables;
+    }
 }
