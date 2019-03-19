ba0f30ef7366ea0537739a043d77e62ef96cf2c3
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-20 00:49:05 +0300

    add classes for rest assured tests

---
+++ src/main/java/homeworks/homework10/specifications/RequestSpecification.java
@@ -0,0 +1,4 @@
+package homeworks.homework10.specifications;
+
+public class RequestSpecification {
+}
---
+++ src/main/java/homeworks/homework10/testResources/TestProperties.java
@@ -0,0 +1,31 @@
+package homeworks.homework10.testResources;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+
+    private static FileInputStream testPropertiesFile;
+    private static Properties testProperties;
+
+    TestProperties() {
+        try {
+            testPropertiesFile = new FileInputStream("test.properties");
+            testProperties = new Properties();
+            testProperties.load(testPropertiesFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (testPropertiesFile != null)
+                try {
+                    testPropertiesFile.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+    public static String getTestProperty(String key) {
+        return testProperties.getProperty(key);
+    }
+}
