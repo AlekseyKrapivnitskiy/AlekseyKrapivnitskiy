24602057655d259d0879ed44ab0eea6edd6cf526
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-20 01:28:33 +0300

    add send request class

---
+++ src/main/java/homeworks/homework10/testResources/SendRequest.java
@@ -0,0 +1,18 @@
+package homeworks.homework10.testResources;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+
+import static homeworks.homework10.testResources.TestProperties.getTestProperty;
+
+public class SendRequest {
+
+    public Response sendGetRequest(HashMap<String, String> params) {
+        return RestAssured.with()
+                .queryParams(params)
+                .log().all()
+                .get(getTestProperty("baseUrl")).prettyPeek();
+    }
+}
