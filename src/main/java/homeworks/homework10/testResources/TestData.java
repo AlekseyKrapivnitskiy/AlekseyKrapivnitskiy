33ba7f392d699fd9234c2ef033f0aa1f1fded2cc
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-22 13:01:34 +0300

    add tests

--- src/main/java/homeworks/homework10/testResources/TestData.java
+++ src/main/java/homeworks/homework10/testResources/TestData.java
@@ -5,10 +5,10 @@ public enum TestData {
     UNKNOWN_WORD_TEST_RU("марковь"),
     UNKNOWN_WORD_TEST_EN("tamoto"),
     UNKNOWN_WORD_TEST_UK("превіт"),
-/*
-    REPEAT_WORD_TEST_RU(),
-    REPEAT_WORD_TEST_EN(),
-    REPEAT_WORD_TEST_UK()*/;
+
+    REPEAT_WORD_TEST_RU("я полетел на на Кипр"),
+    REPEAT_WORD_TEST_EN("lets go go to the north"),
+    REPEAT_WORD_TEST_UK("Привіт як як справи");
 
     public String word;
 
--- src/test/java/homeworks/homework10/errorsTests/RepeatWordTest.java
+++ src/test/java/homeworks/homework10/errorsTests/RepeatWordTest.java
@@ -7,8 +7,9 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 import static homeworks.homework10.enums.Errors.ERROR_UNKNOWN_WORD;
-import static homeworks.homework10.enums.Language.RU;
+import static homeworks.homework10.enums.Language.*;
 import static homeworks.homework10.requests.CheckTexts.checkTexts;
+import static homeworks.homework10.testResources.TestData.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -18,18 +19,47 @@ public class RepeatWordTest {
     public void repeatWordRuTest() {
         //make query params
         QueryParamBuilder queryParams = new QueryParamBuilder();
-        String wordInRequest = "я полетел на на Кипр";
-        queryParams.language(RU).options(8).texts(wordInRequest);
+        queryParams.language(RU).options(8).texts(REPEAT_WORD_TEST_RU.word);
 
         //send request
         Response response = checkTexts(queryParams);
 
         //get data from response
         List<List> code = response.getBody().jsonPath().getList("code");
-        List<List> word = response.getBody().jsonPath().getList("word");
 
         //assertions
         assertThat(code.get(0).get(0), equalTo(ERROR_UNKNOWN_WORD.code));
-        assertThat(word.get(0).get(0), equalTo(wordInRequest));
+    }
+
+    @Test
+    public void repeatWordEnTest() {
+        //make query params
+        QueryParamBuilder queryParams = new QueryParamBuilder();
+        queryParams.language(EN).options(8).texts(REPEAT_WORD_TEST_EN.word);
+
+        //send request
+        Response response = checkTexts(queryParams);
+
+        //get data from response
+        List<List> code = response.getBody().jsonPath().getList("code");
+
+        //assertions
+        assertThat(code.get(0).get(0), equalTo(ERROR_UNKNOWN_WORD.code));
+    }
+
+    @Test
+    public void repeatWordUkTest() {
+        //make query params
+        QueryParamBuilder queryParams = new QueryParamBuilder();
+        queryParams.language(UK).options(8).texts(REPEAT_WORD_TEST_UK.word);
+
+        //send request
+        Response response = checkTexts(queryParams);
+
+        //get data from response
+        List<List> code = response.getBody().jsonPath().getList("code");
+
+        //assertions
+        assertThat(code.get(0).get(0), equalTo(ERROR_UNKNOWN_WORD.code));
     }
 }
