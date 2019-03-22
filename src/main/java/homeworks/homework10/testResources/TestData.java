113c0a4b355127c3c69ad64a9331bb7e264b0421
Aleksey_Krapivnitskiy <krapivnitskiy@secureducation.ru>
2019-03-22 13:06:43 +0300

    add tests

--- src/main/java/homeworks/homework10/testResources/TestData.java
+++ src/main/java/homeworks/homework10/testResources/TestData.java
@@ -8,7 +8,11 @@ public enum TestData {
 
     REPEAT_WORD_TEST_RU("я полетел на на Кипр"),
     REPEAT_WORD_TEST_EN("lets go go to the north"),
-    REPEAT_WORD_TEST_UK("Привіт як як справи");
+    REPEAT_WORD_TEST_UK("Привіт як як справи"),
+
+    CAPITALIZATION_TEST_RU("санкт-петербург"),
+    CAPITALIZATION_TEST_EN("saint petersburg"),
+    CAPITALIZATION_TEST_UK("іван");
 
     public String word;
 
---
+++ src/test/java/homeworks/homework10/errorsTests/CapitalizationTest.java
@@ -0,0 +1,71 @@
+package homeworks.homework10.errorsTests;
+
+import homeworks.homework10.queryParamBuilder.QueryParamBuilder;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static homeworks.homework10.enums.Errors.ERROR_CAPITALIZATION;
+import static homeworks.homework10.enums.Language.*;
+import static homeworks.homework10.requests.CheckTexts.checkTexts;
+import static homeworks.homework10.testResources.TestData.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CapitalizationTest {
+
+    @Test
+    public void capitalizationRuTest() {
+        //make query params
+        QueryParamBuilder queryParams = new QueryParamBuilder();
+        queryParams.language(RU).texts(CAPITALIZATION_TEST_RU.word);
+
+        //send request
+        Response response = checkTexts(queryParams);
+
+        //get data from response
+        List<List> code = response.getBody().jsonPath().getList("code");
+        List<List> word = response.getBody().jsonPath().getList("word");
+
+        //assertions
+        assertThat(code.get(0).get(0), equalTo(ERROR_CAPITALIZATION.code));
+        assertThat(word.get(0).get(0), equalTo(CAPITALIZATION_TEST_RU.word));
+    }
+
+    @Test
+    public void capitalizationEnTest() {
+        //make query params
+        QueryParamBuilder queryParams = new QueryParamBuilder();
+        queryParams.language(EN).texts(CAPITALIZATION_TEST_EN.word);
+
+        //send request
+        Response response = checkTexts(queryParams);
+
+        //get data from response
+        List<List> code = response.getBody().jsonPath().getList("code");
+        List<List> word = response.getBody().jsonPath().getList("word");
+
+        //assertions
+        assertThat(code.get(0).get(0), equalTo(ERROR_CAPITALIZATION.code));
+        assertThat(word.get(0).get(0), equalTo(CAPITALIZATION_TEST_EN.word));
+    }
+
+    @Test
+    public void capitalizationUkTest() {
+        //make query params
+        QueryParamBuilder queryParams = new QueryParamBuilder();
+        queryParams.language(UK).texts(CAPITALIZATION_TEST_UK.word);
+
+        //send request
+        Response response = checkTexts(queryParams);
+
+        //get data from response
+        List<List> code = response.getBody().jsonPath().getList("code");
+        List<List> word = response.getBody().jsonPath().getList("word");
+
+        //assertions
+        assertThat(code.get(0).get(0), equalTo(ERROR_CAPITALIZATION.code));
+        assertThat(word.get(0).get(0), equalTo(CAPITALIZATION_TEST_UK.word));
+    }
+}
